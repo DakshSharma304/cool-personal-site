85a26d4518b6142888d2613acd238e608192bca1
Daksh Sharma <dakshsharmanj@gmail.com>
2026-09-07 21:42:23 -0400

    base

---
+++ script.js
@@ -0,0 +1,7 @@
+const github = document.getElementById("gitVisit");
+
+github.addEventListener("click", redirectGithub);
+
+function redirectGithub() {
+    window.open("https://github.com/DakshSharma304");
+}
